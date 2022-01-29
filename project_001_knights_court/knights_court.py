28a3deeaf2a6b1755022f2d89580f61690ff6d1e
Rhean Propp <rhean.propp@hotmail.com>
2022-01-28 20:10:58 -0700

    Create knights_court.py

---
+++ project_001_knights_court/knights_court.py
@@ -0,0 +1,11 @@
+# Author: Rhean Propp
+# Date: January 28, 2022
+# Desc: Text Adventure Prototype
+# Goal: The game should be fun in its first version of the prototype stage.
+
+# Game Start
+print("Would you like to play a game?\n(yes/no)\n::")
+
+input_buffer = input
+
+#"You awake. The air is cold and damp. You open your eyes as they are met with darkness."
