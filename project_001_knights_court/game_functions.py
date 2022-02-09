1c5e021643c0591daf1656385ec51bdbb0e90da2
Rhean <rhean.propp@hotmail.com>
2022-02-08 19:21:24 -0700

    Progress Lof

--- project_001_knights_court/game_functions.py
+++ project_001_knights_court/game_functions.py
@@ -2,78 +2,103 @@
 
 from time import sleep
 
-def start_game():
+def help():
+    print("\nCommand List:\n\n\thelp\t\t\t\t| Lists commands user can use\n\t\t\t\t\t|\n\tuse <item>\t\t\t| Uses an item from the user's inventory\n\t\t\t\t\t|\n\tinv\t\t\t\t| Displays user's inventory\n\t\t\t\t\t|\n\tmove <direction>\t\t| Move character in specific direction.\n\t\t\t\t\t|\n\tattack <object> with <item>\t| Attack creature/object with item from user inventory\n\t\t\t\t\t|\n")
+
+def start_game():       # Prompt user to start game.
     
-    print("Would you like to play a game?\n")
+    print("\nWould you like to play a game?\n")
     
     input_buffer = input("> ").lower()
 
     if "yes" in input_buffer:
-
-        print("Starting game...")
+        sleep(1)
+        print("\nStarting game...\n")
         sleep(2)
+        # Add call to begin game.
     
     elif "no" in input_buffer:
 
-        print("Maybe another time..")
+        sleep(1)
+        print("\nMaybe another time..")
     
     elif "perhaps" in input_buffer:
 
-        print("Perhaps yes or perhaps no?")
+        sleep(3)
+        print("\nPerhaps yes or perhaps no?\n")
         
         input_buffer = input("> ").lower()
 
         if "perhaps" in input_buffer:
+            
+            sleep(3)
 
-            print("No really. Do you want to play this game or not?")
+            print("\nNo really. Do you want to play this game or not?\n")
             input_buffer = input("> ").lower()
         
         if "yes" in input_buffer:
 
-            print("Starting game...")
+            sleep(1)
+            print("\nStarting game...\n")
             sleep(2)
+            # Add call to begin game
 
         elif "no" in input_buffer:
 
-            print("Ok. Fine. Have it your way.")
+            sleep(1)
+            print("\nOk. Fine. Have it your way.\n")
             exit()
 
         elif "perhaps" in input_buffer:
 
-            sleep(4.5)
-            print("\n." end="")
-            sleep(1)
-            print("." end="")       # fix
-            sleep(1)
+            sleep(3)
+            print("\n.", end="", flush=True)
+            sleep(0.15)
+            print(".", end="", flush=True)       # fix
+            sleep(0.15)
             print(".\n")
             sleep(4)
-            print("Ok you know what?\n")
+            print("Ok, you know what?\n")
             sleep(2.5)
             print("Fine.\n")
-            sleep(1.5)
-            print("PERHAPS\n")
             sleep(1.8)
-            print("Apparently you think i'm more than some binary computer.\n")
+            print("PERHAPS\n")
+            sleep(3.8)
+            print("APPARENTLY you think i'm more than some binary computer.\n")
             sleep(4)
-            print("Maybe you think. OH. THIS IS A QUANTUM COMPUTER. HE CAN HANDLE PERHAPS.\n")
+            print("Perhaps you think:\n")
+            sleep(0.8)
+            print("Oh!\n")
+            sleep(1.6)
+            print("This is a quantum computer!\n")
+            sleep(1.7)
+            print("HE CAN HANDLE PERHAPS\n")
             sleep(4)
-            print("WELL PERHAPS YOU'RE WRONG\n")
-            sleep(3)
-            print("Perhaps things didn't work out the way you expected them to.\n")
+            print("WELL\n")
+            sleep(0.5)
+            print("PERHAPS\n")
+            sleep(0.5)
+            print("YOU'RE\n")
+            sleep(0.5)
+            print("WRONG\n")
+            sleep(3.4)
+            print("PERHAPS THINGS DIDNT WORK OUT THE WAY YOU EXPECTED THEM TO\n")
             sleep(3.5)
-            print("You've just been PERHAPSING your whole life, HAVENT YOU?\n")
+            print("You've just been PERHAPS-ING your whole life, HAVENT YOU?\n")
             sleep(3)
             print("Well you know what?\n")
             sleep(2)
             print("PERHAPS I LEAVE!\n")
-            sleep(1)
+            sleep(1.5)
             print("PERHAPS THAT!\n")
             exit()
 
         else:
 
-            print("You're really not making this easy for me, are you?")
+            print("\nYou're really not making this easy for me, are you?\n")
     else:
 
-        print("It's a yes or no question.")
+        sleep(1)
+        print("\nIt's a yes or no question.")
+        sleep(3)
         start_game()
